016fb2a69ddf27a021cbc4640d036d710129310b
Bianca <103680194+biancadearaujo@users.noreply.github.com>
2023-07-30 16:55:37 -0300

    banco de dados

---
+++ SistemaSkill.sql
@@ -0,0 +1,17 @@
+﻿CREATE DATABASE sistema_skill;
+
+CREATE TABLE login (
+    id_login SERIAL PRIMARY KEY,
+    login VARCHAR(30) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE skill (
+    id_skill SERIAL PRIMARY KEY,
+    skill VARCHAR(255) NOT NULL,
+    level VARCHAR(255) NOT NULL,
+    id_login INT,
+    FOREIGN KEY (id_login) REFERENCES login(id_login)
+);
+
+INSERT INTO login (login, senha) VALUES ('usuario1', '123');
